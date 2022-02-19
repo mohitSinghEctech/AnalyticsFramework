141e0e0e9465108bd2e517a2bc4285581a979acc
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-02-19 15:33:30 +0530

    added pod workspace

---
+++ Pods/Target Support Files/Pods-AnalyticFramework-AnalyticFrameworkTests/Pods-AnalyticFramework-AnalyticFrameworkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnalyticFramework_AnalyticFrameworkTests : NSObject
+@end
+@implementation PodsDummy_Pods_AnalyticFramework_AnalyticFrameworkTests
+@end
---
+++ Pods/Target Support Files/Pods-AnalyticFramework/Pods-AnalyticFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnalyticFramework : NSObject
+@end
+@implementation PodsDummy_Pods_AnalyticFramework
+@end
