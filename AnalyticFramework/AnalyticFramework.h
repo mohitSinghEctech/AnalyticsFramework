397c04573b5829db83131e8b98a079c55883556e
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-02-19 15:27:19 +0530

    added framework

---
+++ AnalyticFramework/AnalyticFramework.h
@@ -0,0 +1,18 @@
+//
+//  AnalyticFramework.h
+//  AnalyticFramework
+//
+//  Created by Mohit Kumar Singh on 19/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticFramework.
+FOUNDATION_EXPORT double AnalyticFrameworkVersionNumber;
+
+//! Project version string for AnalyticFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticFramework/PublicHeader.h>
+
+
---
+++ AnalyticFramework/Analytics.swift
@@ -0,0 +1,16 @@
+//
+//  Analytics.swift
+//  AnalyticFramework
+//
+//  Created by Mohit Kumar Singh on 19/02/22.
+//
+
+import Foundation
+
+public class Analytics {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
